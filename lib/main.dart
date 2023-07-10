ec9796db3d7149e7f917ef8dfde610209dd2f007
민감자 <mouse0429@naver.com>
2023-07-10 17:00:16 +0900

    chore: dotenv 파일 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,11 @@ import 'package:amplitude_flutter/amplitude.dart';
 import 'package:blabla/services/amplitude.dart';
 import 'package:blabla/styles/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
+
   runApp(const MyApp());
 }
 
