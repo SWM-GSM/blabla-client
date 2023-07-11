6c5413ac20e037ca97ec607f3b575308182aaee0
민감자 <mouse0429@naver.com>
2023-07-12 03:53:14 +0900

    chore: 다국어 언어 설정
    - minSdkVersion 28로 변경
    - 한국어, 영어 파일 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:amplitude_flutter/amplitude.dart';
+import 'package:blabla/screens/join/join_birthdate_view.dart';
 import 'package:blabla/screens/join/join_nickname_view.dart';
 import 'package:blabla/screens/join/join_profile_view.dart';
 import 'package:blabla/screens/join/join_profile2_view.dart';
@@ -6,24 +7,27 @@ import 'package:blabla/screens/join/join_view_model.dart';
 import 'package:blabla/screens/onboarding.dart';
 import 'package:blabla/services/amplitude.dart';
 import 'package:blabla/styles/theme.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await dotenv.load();
 
   AnalyticsConfig().init();
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => JoinViewModel())
-      ],
-      child: const MyApp()
+    EasyLocalization(
+      supportedLocales: const [Locale('en'), Locale('ko')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en'),
+      child: MultiProvider(
+          providers: [ChangeNotifierProvider(create: (_) => JoinViewModel())],
+          child: const MyApp()),
     ),
   );
-    
 }
 
 class MyApp extends StatelessWidget {
@@ -32,9 +36,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'BlaBla',
-      theme: BlaTheme.blaTheme,
-      home: JoinNicknameView()//OnBoarding(),
-    );
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        title: 'BlaBla',
+        theme: BlaTheme.blaTheme,
+        home: JoinBirthdateView() //OnBoarding(),
+        );
   }
 }
