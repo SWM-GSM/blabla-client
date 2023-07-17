1a507a665e368c924896858ee5def729d8e31f2e
민감자 <mouse0429@naver.com>
2023-07-18 02:35:07 +0900

    feat: 크루생성 - 크루 모임 주기 화면

--- lib/screens/recruit/recruit_view_model.dart
+++ lib/screens/recruit/recruit_view_model.dart
@@ -22,14 +22,30 @@ enum CrewProfile {
   book,
 }
 
+enum CrewCycle {
+  free("자율", "FREE"),
+  onceAMonth("월 1회", "ONCEAMONTH"),
+  twiceAMonth("월 2회", "TWICEAMONTH"),
+  onceAWeek("주 1회", "ONCEAWEEK"),
+  twiceAWeek("주 2~3회", "TWICEAWEEK"),
+  fourTimesAWeek("주 4~5회", "FOURTIMESAWEEK"),
+  everyday("매일", "EVERYDAY");
+  
+  const CrewCycle(this.name, this.tag);
+  final String name;
+  final String tag;
+}
+
 class RecruitViewModel with ChangeNotifier {
   late String _profileImg;
   late String _name;
   late String _desc;
+  CrewCycle? _cycle;
 
   String get profileImg => _profileImg;
   String get name => _name;
   String get desc => _desc;
+  CrewCycle? get cycle => _cycle;
 
   RecruitViewModel() {
     changeProfile();
@@ -44,13 +60,14 @@ class RecruitViewModel with ChangeNotifier {
         initName();
       case RecruitPage.desc:
         initDesc();
+      case RecruitPage.cycle:
+        initCycle();
       default:
         break;
     }
     notifyListeners();
   }
-  
-  
+
   void changeProfile() {
     int idx = Random().nextInt(CrewProfile.values.length);
     _profileImg = CrewProfile.values[idx].name;
@@ -59,17 +76,31 @@ class RecruitViewModel with ChangeNotifier {
 
   void initName() {
     _name = "";
+    notifyListeners();
   }
 
   void setName(String input) {
     _name = input;
+    notifyListeners();
   }
 
   void initDesc() {
     _desc = "";
+    notifyListeners();
   }
 
   void setDesc(String input) {
     _desc = input;
+    notifyListeners();
+  }
+
+  void initCycle() {
+    _cycle = null;
+    notifyListeners();
+  }
+
+  void setCycle(int idx) {
+    _cycle = CrewCycle.values[idx];
+    notifyListeners();
   }
 }
