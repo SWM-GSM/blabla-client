c6de34672ae5362322b8ab4558670865600cec9b
민감자 <mouse0429@naver.com>
2023-08-11 05:08:54 +0900

    feat: 마이스페이스 스피킹 화면

---
+++ lib/screens/my_space/mys_view_model.dart
@@ -0,0 +1,29 @@
+import 'package:blabla/models/content_feedback.dart';
+import 'package:blabla/screens/my_space/widgets/mys_content_record_widget.dart';
+import 'package:blabla/services/apis/api.dart';
+import 'package:flutter/material.dart';
+
+class MysViewModel with ChangeNotifier {
+  final api = API();
+
+  int _contentId = 0;
+  ContentFeedback? _feedback;
+
+  /* 스피킹 */
+  List<String> _recordPathes = List.generate(3, (idx) => "");
+  List<String> get recordPathes => _recordPathes;
+
+
+  MysViewModel() {}
+
+  void setContentId(int id) {
+    _contentId = id;
+    notifyListeners();
+  }
+
+
+  void setRecordPath(int idx, String path) {
+    _recordPathes[idx] = path;
+    notifyListeners();
+  }
+}
