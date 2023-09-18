33bade42915b65c6724a84ad81fc99db34bcc5d4
민감자 <mouse0429@naver.com>
2023-09-19 04:49:43 +0900

    chore: report_personal_view 번역 적용 및 뒤로가기 방지

--- lib/screens/profile/report_personal_view.dart
+++ lib/screens/profile/report_personal_view.dart
@@ -2,6 +2,7 @@ import 'package:blabla/screens/practice/practice_video_speaking_view.dart';
 import 'package:blabla/screens/profile/profile_view_model.dart';
 import 'package:blabla/styles/colors.dart';
 import 'package:blabla/styles/txt_style.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
@@ -12,131 +13,136 @@ class ReportPersonalView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final viewModel = Provider.of<ProfileViewModel>(context);
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 64,
-        title: Text(
-          "피드백",
-          style: BlaTxt.txt18B,
-        ),
-        backgroundColor: BlaColor.white,
-        elevation: 0,
-        centerTitle: true,
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-            viewModel.initPersonalReport();
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: SvgPicture.asset(
-              "assets/icons/ic_32_arrow_left.svg",
-              width: 24,
-              height: 24,
+    return WillPopScope(
+      onWillPop: () async {
+        viewModel.initPersonalReport();
+        return true;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 64,
+          title: Text(
+            "feedback".tr(),
+            style: BlaTxt.txt18B,
+          ),
+          backgroundColor: BlaColor.white,
+          elevation: 0,
+          centerTitle: true,
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pop(context);
+              viewModel.initPersonalReport();
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: SvgPicture.asset(
+                "assets/icons/ic_32_arrow_left.svg",
+                width: 24,
+                height: 24,
+              ),
             ),
           ),
+          leadingWidth: 64,
+          actions: [
+            Container(
+              width: 64,
+              color: Colors.transparent,
+            ),
+          ],
         ),
-        leadingWidth: 64,
-        actions: [
-          Container(
-            width: 64,
-            color: Colors.transparent,
-          ),
-        ],
-      ),
-      body: viewModel.feedback == null
-          ? Padding(
-              padding: EdgeInsets.only(
-                  bottom: 90 + MediaQuery.of(context).padding.bottom),
-              child: const Center(
-                  child: CircularProgressIndicator(
-                color: BlaColor.orange,
-              )),
-            )
-          : Padding(
-              padding: EdgeInsets.fromLTRB(
-                  20, 0, 20, 90 + MediaQuery.of(context).padding.bottom),
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        color: BlaColor.grey100,
-                        borderRadius: BorderRadius.circular(12),
+        body: viewModel.feedback == null
+            ? Padding(
+                padding: EdgeInsets.only(
+                    bottom: 90 + MediaQuery.of(context).padding.bottom),
+                child: const Center(
+                    child: CircularProgressIndicator(
+                  color: BlaColor.orange,
+                )),
+              )
+            : Padding(
+                padding: EdgeInsets.fromLTRB(
+                    20, 0, 20, 90 + MediaQuery.of(context).padding.bottom),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: BlaColor.grey100,
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "sentenceSimilarity".tr(),
+                                  style: BlaTxt.txt20B,
+                                ),
+                                starScore(viewModel.feedback!.starRating),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            Text("myTranslatedSentence".tr(),
+                                style: BlaTxt.txt12R
+                                    .copyWith(color: BlaColor.grey700)),
+                            const SizedBox(height: 4),
+                            Text(viewModel.feedback!.userAnswer,
+                                style: BlaTxt.txt16B
+                                    .copyWith(overflow: TextOverflow.visible)),
+                            const SizedBox(height: 16),
+                            Text("correctSentence".tr(),
+                                style: BlaTxt.txt12R
+                                    .copyWith(color: BlaColor.grey700)),
+                            const SizedBox(height: 4),
+                            Text(viewModel.feedback!.targetSentence,
+                                style: BlaTxt.txt16B
+                                    .copyWith(overflow: TextOverflow.visible)),
+                          ],
+                        ),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "문장 유사도",
-                                style: BlaTxt.txt20B,
-                              ),
-                              starScore(viewModel.feedback!.starRating),
-                            ],
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          Text("내가 영작한 문장",
-                              style: BlaTxt.txt12R
-                                  .copyWith(color: BlaColor.grey700)),
-                          const SizedBox(height: 4),
-                          Text(viewModel.feedback!.userAnswer,
-                              style: BlaTxt.txt16B
-                                  .copyWith(overflow: TextOverflow.visible)),
-                          const SizedBox(height: 16),
-                          Text("정답 문장",
-                              style: BlaTxt.txt12R
-                                  .copyWith(color: BlaColor.grey700)),
-                          const SizedBox(height: 4),
-                          Text(viewModel.feedback!.targetSentence,
-                              style: BlaTxt.txt16B
-                                  .copyWith(overflow: TextOverflow.visible)),
-                        ],
+                      const SizedBox(height: 20),
+                      Text(
+                        viewModel.feedback!.longFeedback,
+                        style: BlaTxt.txt14R.copyWith(
+                            color: BlaColor.grey800,
+                            overflow: TextOverflow.visible),
                       ),
-                    ),
-                    const SizedBox(height: 20),
-                    Text(
-                      viewModel.feedback!.longFeedback,
-                      style: BlaTxt.txt14R.copyWith(
-                          color: BlaColor.grey800,
-                          overflow: TextOverflow.visible),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
+        bottomSheet: Container(
+          padding: EdgeInsets.fromLTRB(
+              20, 12, 20, 12 + MediaQuery.of(context).padding.bottom),
+          decoration: const BoxDecoration(
+            border: Border(
+              top: BorderSide(color: BlaColor.grey100, width: 1),
             ),
-      bottomSheet: Container(
-        padding: EdgeInsets.fromLTRB(
-            20, 12, 20, 12 + MediaQuery.of(context).padding.bottom),
-        decoration: const BoxDecoration(
-          border: Border(
-            top: BorderSide(color: BlaColor.grey100, width: 1),
+            color: BlaColor.white,
           ),
-          color: BlaColor.white,
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const PracticeVideoSpeakingView(),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const PracticeVideoSpeakingView(),
+                ),
+              );
+            },
+            child: Container(
+              alignment: Alignment.center,
+              height: 56,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(12),
+                color: BlaColor.orange,
               ),
-            );
-          },
-          child: Container(
-            alignment: Alignment.center,
-            height: 56,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(12),
-              color: BlaColor.orange,
+              child: Text("continue".tr(),
+                  style: BlaTxt.txt16B.copyWith(color: BlaColor.white)),
             ),
-            child: Text("계속하기",
-                style: BlaTxt.txt16B.copyWith(color: BlaColor.white)),
           ),
         ),
       ),
