02c14120454edef52e420a8acae739402c1e36c0
민감자 <mouse0429@naver.com>
2023-09-18 14:49:25 +0900

    fix: 바텀 네비게이션 시작 위치 변경

--- lib/providers/nav_provider.dart
+++ lib/providers/nav_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NavProvider with ChangeNotifier {
-  int _curIdx = 0;
+  int _curIdx = 1;
   int get curIdx => _curIdx;
   
   void changeIdx(int idx) {
