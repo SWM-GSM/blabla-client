95fa633a16039d805085e6cc754c38acc9bb1c3f
민감자 <mouse0429@naver.com>
2023-10-11 16:09:49 +0900

    chore: 초기 가입 유저 알림 설정 허용

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,13 +1,11 @@
 import 'package:blabla/main.dart';
 import 'package:blabla/screens/onboarding.dart';
-import 'package:blabla/screens/profile/profile_view_model.dart';
 import 'package:blabla/services/apis/api.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:provider/provider.dart';
 
 enum SplashPage { onboarding, home }
 
@@ -41,18 +39,26 @@ class _SplashState extends State<Splash> {
       });
     }
 
+    print(
+        "accessToken: $accessToken / refresToken: $refreshToken /  accessExpireDate: $accessExpireDate / refreshExpireDate: $refreshExpireDate");
     if (accessToken == null ||
         refreshToken == null ||
         accessExpireDate == null ||
         refreshExpireDate == null) {
-      print(
-          "$accessToken, $refreshToken, $accessExpireDate, $refreshExpireDate");
       setState(() {
         page = SplashPage.onboarding;
       });
     } else {
-      print(
-          "accessToken: $accessToken / refresToken: $refreshToken /  accessExpireDate: $accessExpireDate / refreshExpireDate: $refreshExpireDate");
+      if (!await Permission.notification.isGranted) {
+        // 푸쉬 알림 권한 설정
+        if ((await Permission.notification.request()) ==
+            PermissionStatus.granted) {
+          await API().patchAllowNotification(true);
+        } else {
+          await API().patchAllowNotification(false);
+        }
+      }
+
       if (DateTime.fromMillisecondsSinceEpoch(int.parse(
               refreshExpireDate)) // refreshToken이 만료시간까지 4시간보다 넉넉하게 남아있지 않을 경우 재로그인 필요
           .isBefore(DateTime.now().add(const Duration(hours: 4)))) {
@@ -71,19 +77,6 @@ class _SplashState extends State<Splash> {
             setState(() {
               page = SplashPage.home;
             });
-            if (!await Permission.notification.isGranted) {
-              WidgetsBinding.instance.addPostFrameCallback((_) async {
-                final profileViewModel = Provider.of<ProfileViewModel>(context);
-                if ((await Permission.notification.request()) ==
-                    PermissionStatus.granted) {
-                  await API().patchAllowNotification(true);
-                  profileViewModel.initProfile();
-                } else {
-                  await API().patchAllowNotification(false);
-                  profileViewModel.initProfile();
-                }
-              });
-            }
           }
         } else {
           setState(() {
@@ -103,7 +96,7 @@ class _SplashState extends State<Splash> {
       case SplashPage.home:
         return const Main();
       default:
-        return const Placeholder();
+        return const OnBoarding();
     }
   }
 }
